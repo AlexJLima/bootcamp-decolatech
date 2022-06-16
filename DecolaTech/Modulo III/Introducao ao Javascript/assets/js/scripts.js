53c640a1535856f4d6270e4f26a26d4474fa84dc
AlexJLima <alexandrejefferson@outlook.com>
2022-06-16 18:05:52 -0300

    Implementação dos eventos utilizando o método addEventListener

--- DecolaTech/Modulo III/Introducao ao Javascript/assets/js/scripts.js
+++ DecolaTech/Modulo III/Introducao ao Javascript/assets/js/scripts.js
@@ -1,22 +1,17 @@
-var currentNumberWrapper = document.getElementById('currentNumber');
-var currentNumber = 0
+var botaoSubtrair = document.getElementById('subtrair');
+var botaoAdicionar = document.getElementById('adicionar');
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+botaoSubtrair.addEventListener("click", decrement);
+botaoAdicionar.addEventListener("click", increment);
 
 function increment() {
-    currentNumber += 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-    changeColor();
+   currentNumber += 1;
+   currentNumberWrapper.innerHTML = currentNumber;
 }
 
 function decrement() {
     currentNumber -= 1;
     currentNumberWrapper.innerHTML = currentNumber;
-    changeColor();
-}
-
-function changeColor() {
-    if (currentNumberWrapper.innerHTML < '0') {
-        currentNumberWrapper.style.color = 'red';
-    } else {
-        currentNumberWrapper.style.color = 'black';
-    }
 }
