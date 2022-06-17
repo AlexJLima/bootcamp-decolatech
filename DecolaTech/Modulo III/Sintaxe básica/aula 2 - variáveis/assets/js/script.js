057387950c9162d5250eed8059095a319f20d682
AlexJLima <alexandrejefferson@outlook.com>
2022-06-17 20:07:19 -0300

    Arquivos da aula 2 sobre variáveis

---
+++ DecolaTech/Modulo III/Sintaxe básica/aula 2 - variáveis/assets/js/script.js
@@ -0,0 +1,96 @@
+// tipos primitivos
+
+// boolean
+
+/*var verdadeiroOuFalso = false;
+console.log(typeof(verdadeiroOuFalso));
+
+// number
+
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'alexandre';
+console.log(typeof(nome));*/
+
+/*var variavel = 'alex';
+variavel = 'alexandre';
+console.log(variavel);*/
+
+/*let variavel2 = 'alex';
+variavel2 ='alexandre';
+console.log(variavel2);*/
+
+/*const constante = 'alexandre';
+console.log(constante);*/
+
+/*var escopoGlobal = 'Global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal();*/
+
+/*// atribuição
+var atribuicao = 'alex';
+
+// comparação
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+// comparação identica
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica);*/
+
+/*// adicao
+var adicao = 1 + 1;
+console.log(adicao);
+
+// subração
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+// multiplicação 
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+// divisão real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+// divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// potenciação
+var potenciacao = 2 ** 10;
+console.log(potenciacao);*/
+
+/*// maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+// menor 
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual a 
+var maiorOuIgual = 5 >= 2;
+console.log(maiorOuIgual);
+
+// menor ou igual a 
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual);*/
+
+/*var e = true && false;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);*/
