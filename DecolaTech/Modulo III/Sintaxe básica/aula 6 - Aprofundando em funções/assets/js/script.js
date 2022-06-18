924bc5a9c34b70aac8f3aa4344d515543b7da4f2
AlexJLima <alexandrejefferson@outlook.com>
2022-06-17 22:00:58 -0300

    Aula 6 sobre mais detalhes das funções

---
+++ DecolaTech/Modulo III/Sintaxe básica/aula 6 - Aprofundando em funções/assets/js/script.js
@@ -0,0 +1,24 @@
+// tipos de funções
+
+// declarativa
+
+/* function funcao() {
+    console.log('Mensagem de uma função declarativa');
+}
+
+funcao(); */
+
+// expressão de função
+// com nomeação
+
+/* var funcao = function () {
+    console.log('Sou uma mensagem de função de expressão');
+}
+
+funcao(); */
+
+/* var funcao = () => {
+    console.log('Sou uma array function');
+}
+
+funcao(); */
