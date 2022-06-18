09467f0fcc3f98d62d0a6072b56ebea98f4304f1
AlexJLima <alexandrejefferson@outlook.com>
2022-06-17 21:19:18 -0300

    Aula 5 sobre funçoes

---
+++ DecolaTech/Modulo III/Sintaxe básica/aula 5 - Funções/assets/js/script.js
@@ -0,0 +1,10 @@
+/* function funcao() {
+    console.log('Vida longa e próspera');
+}
+funcao(); */
+
+/* function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo);
+}
+
+mensagem('Vida longa', 'e próspera'); */
