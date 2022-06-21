52f1bf2fec66042c94684e32c58790fff985b3d6
AlexJLima <alexandrejefferson@outlook.com>
2022-06-21 09:04:26 -0300

    Aula sobre SafeAreaView e primeiros experimentos

--- decolatech/modulo-iv/introducao-ao-react-native/project/src/App.js
+++ decolatech/modulo-iv/introducao-ao-react-native/project/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, StatusBar, Image} from 'react-native';
 import { registerRootComponent } from 'expo';
+
+const colorGitHub = '#010409';
+const colorFontGitHub = '#C9D1D9';
+const colorDarkFontGitHub = '#4F565E';
+const imageGitHubProfile = 'https://raw.githubusercontent.com/1l0/identicon/master/example/identicons/normal_white.png';
 
 const App = () => {
     return (
-        <View style={style.container}> 
-            <Text style={style.text}> Alexandre Lima</Text> 
-        </View>
+            <SafeAreaView style={style.container}>
+                <StatusBar backgroundColor={colorGitHub} barStyle='light-content'/>
+                    <View style={style.content}> 
+                        <Image source={{uri: imageGitHubProfile}} style={style.avatar}/>
+                    </View>
+                    <Text style={[style.defaultText, style.name]}>Alexandre Lima</Text>
+                    <Text style={[style.defaultText, style.nickname]}>AlexJLima</Text>
+            </SafeAreaView>
     );
 };
 
@@ -15,14 +25,33 @@ export default registerRootComponent(App);
 const style = StyleSheet.create(
     {
         container:{
-            backgroundColor: 'pink',
+            backgroundColor: colorGitHub,
             flex: 1,
+            alignItems: 'center',
+        },
+        content:{
+            alignItems: 'center',
+            padding: 20,
+        },
+        avatar:{
+            height: 200,
+            width: 200,
+            borderRadius: 100,
+            borderColor: 'white',
+            borderWidth: 2,
+        },
+        defaultText: {
+            color: colorFontGitHub,
         },
 
-        text:{
-            fontSize: 50,
+        name: { 
+            marginTop: 20,
             fontWeight: 'bold',
-            color: 'black',
+            fontSize: 24,
+        },
+        nickname: {
+            fontSize: 18,
+            color: colorDarkFontGitHub,
         },
     }
 );
